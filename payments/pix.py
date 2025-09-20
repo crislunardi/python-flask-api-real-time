820dd078089b71522215b0e633954375dc52beaa
Cristian Lunardi <c.lunarditi@gmail.com>
2025-09-20 14:16:44 -0300

    feat: add qr code generation route

--- payments/pix.py
+++ payments/pix.py
@@ -7,10 +7,14 @@ class Pix:
 
     def create_payment(self):
         bank_payment = str(uuid.uuid4())
-        hash_payment = f"hash_payment{bank_payment}"
+
+        hash_payment = f"hash_payment_{bank_payment}"
+
         img = qrcode.make(hash_payment)
 
-        img.save(f"static/img/qr_code_payment_{bank_payment}.png")
+        img.save(f"static/img/{bank_payment}.png")
 
-        return {"bank_payment": bank_payment,
-                "qr_code_path": f"qr_code_payment_{bank_payment}"}
+        return {
+            "bank_payment":  bank_payment,
+            "qr_code_path": bank_payment
+            }
