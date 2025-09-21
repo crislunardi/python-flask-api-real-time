a6a72a1dfe51b71da253159c5380947b6f5e6ec7
Cristian Lunardi <c.lunarditi@gmail.com>
2025-09-21 12:24:18 -0300

    feat: add confirmation pix route

--- db_models/payment.py
+++ db_models/payment.py
@@ -4,7 +4,7 @@ class Payment(db.Model):
     # id, value, paid, bank_payment_id, qr_code, expiration_date
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float)
-    paid = db.Column(db.Boolean, default=True)
+    paid = db.Column(db.Boolean, default=False)
     bank_payment = db.Column(db.String(100), nullable=True)
     qr_code = db.Column(db.String(100), nullable=True)
     expiration_date = db.Column(db.DateTime)
